e1bf41b050115d5160680a6f9a8ae39af63b6477
songhee24 <akaratel61@gmail.com>
2023-08-10 15:52:31 +0600

    Add comment component

---
+++ src/components/AddComment/index.js
@@ -0,0 +1,27 @@
+import { Avatar, Button, TextField } from "@mui/material";
+import styles from "./AddComment.module.scss";
+
+const AddComment = () => {
+  return (
+    <>
+      <div className={styles.root}>
+        <Avatar
+          classes={{ root: styles.avatar }}
+          src="https://mui.com/static/images/avatar/5.jpg"
+        />
+        <div className={styles.form}>
+          <TextField
+            label="Написать комментарий"
+            variant="outlined"
+            maxRows={10}
+            multiline
+            fullWidth
+          />
+          <Button variant="contained">Отправить</Button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AddComment;
