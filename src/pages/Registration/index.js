688a23d07420ea812ba93bcb11a658f5f374ef39
songhee24 <akaratel61@gmail.com>
2023-08-11 21:51:23 +0600

    regsiter component

---
+++ src/pages/Registration/index.js
@@ -0,0 +1,25 @@
+import Paper from "@mui/material/Paper";
+import { Avatar, Typography } from "@mui/material";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+
+import styles from "./Login.module.scss";
+
+export const Registration = () => {
+  return (
+    <Paper classes={{ root: styles.root }}>
+      <Typography classes={{ root: styles.title }} variant="h5">
+        Создание аккаунта
+      </Typography>
+      <div className={styles.avatar}>
+        <Avatar sx={{ width: 100, height: 100 }} />
+      </div>
+      <TextField className={styles.field} label="Полное имя" fullWidth />
+      <TextField className={styles.field} label="E-Mail" fullWidth />
+      <TextField className={styles.field} label="Пароль" fullWidth />
+      <Button size="large" variant="contained" fullWidth>
+        Зарегистрироваться
+      </Button>
+    </Paper>
+  );
+};
