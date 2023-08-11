7328622335fb5a5d601c7e7f5db245133777031d
songhee24 <akaratel61@gmail.com>
2023-08-11 21:57:55 +0600

    configuring main js

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { theme } from "../theme.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
