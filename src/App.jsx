77ffbf9c1221c54424fa737bae4cb59b71d24eec
songhee24 <akaratel61@gmail.com>
2023-08-11 22:54:48 +0600

    configuring routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,27 @@
 import "./App.css";
 import Header from "./components/Header/index.jsx";
 import { Container } from "@mui/material";
-import { Home } from "./pages/index.jsx";
+import {
+  AddPost,
+  FullPost,
+  Home,
+  Login,
+  Registration,
+} from "./pages/index.jsx";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Header />
       <Container maxWidth="lg">
-        <Home />
-        {/*<FullPost />*/}
-        {/*<AddPost />*/}
-        {/*<Login />*/}
-        {/*<Registration />*/}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts/:id" element={<FullPost />} />
+          <Route path="/add-post" element={<AddPost />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Registration />} />
+        </Routes>
       </Container>
     </>
   );
