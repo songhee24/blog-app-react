50b41c11c1b3eea483a4f203dd3508c8cd2428de
songhee24 <akaratel61@gmail.com>
2023-08-11 23:01:33 +0600

    configure store

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,17 @@ import "./index.scss";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "../theme.js";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <CssBaseline />
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
       </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
