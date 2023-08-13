62e75ae14d10e2182c81c9b4defe2893293cd9f4
songhee24 <akaratel61@gmail.com>
2023-08-14 00:19:30 +0600

    get one post by id

--- src/pages/FullPost.jsx
+++ src/pages/FullPost.jsx
@@ -1,32 +1,43 @@
 import Post from "../components/Post/index.jsx";
 import { CommentsBlock } from "../components/index.js";
 import AddComment from "../components/AddComment/index.jsx";
+import { useParams } from "react-router-dom";
+import React from "react";
+import axios from "../api/axios.js";
 
 export const FullPost = () => {
+  const [data, setData] = React.useState({});
+  const [loading, setLoading] = React.useState(true);
+  const { id } = useParams();
+
+  React.useEffect(() => {
+    axios
+      .get(`/posts/${id}`)
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch(() => alert("Ошибка что то пошло не так"))
+      .finally(() => setLoading(false));
+  }, []);
+
+  if (loading) {
+    return <Post isLoading={loading} />;
+  }
+
   return (
     <>
       <Post
-        id={1}
-        title="Roast the code #1 | Rock Paper Scissors"
-        imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-        user={{
-          avatarUrl:
-            "https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png",
-          fullName: "Keff",
-        }}
-        createdAt={"12 июня 2022 г."}
-        viewsCount={150}
+        _id={data._id}
+        title={data.title}
+        imageUrl={data.imageUrl}
+        user={data.user}
+        createdAt={data.createdAt}
+        viewsCount={data.viewsCount}
         commentsCount={3}
-        tags={["react", "fun", "typescript"]}
+        tags={data.tags}
         isFullPost
       >
-        <p>
-          Hey there! 👋 I'm starting a new series called "Roast the Code", where
-          I will share some code, and let YOU roast and improve it. There's not
-          much more to it, just be polite and constructive, this is an exercise
-          so we can all learn together. Now then, head over to the repo and
-          roast as hard as you can!!
-        </p>
+        <p>{data.text}</p>
       </Post>
       <CommentsBlock
         items={[
