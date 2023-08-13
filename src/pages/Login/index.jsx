3b137650a0aa84708dabb18672cd6c6b554b0717
songhee24 <akaratel61@gmail.com>
2023-08-14 00:47:57 +0600

    using hook forms for validation

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,25 +2,50 @@ import Paper from "@mui/material/Paper";
 import { Typography } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import { useForm } from "react-hook-form";
 import styles from "./Login.module.scss";
 
 export const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
         Вход в аккаунт
       </Typography>
-      <TextField
-        className={styles.field}
-        label="E-Mail"
-        error
-        helperText="Неверно указана почта"
-        fullWidth
-      />
-      <TextField className={styles.field} label="Пароль" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
-        Войти
-      </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          className={styles.field}
+          label="E-Mail"
+          error
+          helperText="Неверно указана почта"
+          {...register("email", { required: "Укажите почту" })}
+          fullWidth
+        />
+        <TextField
+          className={styles.field}
+          label="Пароль"
+          fullWidth
+          {...register("password", { required: "Укажите пароль" })}
+        />
+        <Button size="large" variant="contained" fullWidth>
+          Войти
+        </Button>
+      </form>
     </Paper>
   );
 };
