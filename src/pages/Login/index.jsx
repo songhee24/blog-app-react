fce0e8d02823b84f3ed6f80154af0743d249f0f3
songhee24 <akaratel61@gmail.com>
2023-08-14 13:43:28 +0600

    save token after requets

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -24,8 +24,16 @@ export const Login = () => {
     mode: "all",
   });
 
-  const onSubmit = (values) => {
-    dispatch(fetchAuth(values));
+  const onSubmit = async (values) => {
+    const data = await dispatch(fetchAuth(values));
+
+    if (!data.payload) {
+      return alert("Не удалось авторизоваться!");
+    }
+
+    if ("token" in data.payload) {
+      window.localStorage.setItem("token", data.payload.token);
+    }
   };
 
   if (isAuth) {
