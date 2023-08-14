d50a9756fc405ccb07a956fe1156d9e15d261930
songhee24 <akaratel61@gmail.com>
2023-08-14 23:56:53 +0600

    get posts request logic

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,18 @@ import React from "react";
 import { Grid, Tab, Tabs } from "@mui/material";
 import Post from "../components/Post/index.jsx";
 import { CommentsBlock, TagsBlock } from "../components/index.js";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchPosts } from "../redux/slices/postsSlice.js";
 
 export const Home = () => {
   const dispatch = useDispatch();
+  const { posts, tags } = useSelector((state) => state.reducer);
 
   React.useEffect(() => {
     dispatch(fetchPosts());
   }, []);
+
+  const isPostsLoading = posts.status === "loading";
 
   return (
     <>
@@ -24,23 +27,24 @@ export const Home = () => {
       </Tabs>
       <Grid container spacing={4}>
         <Grid xs={8} item>
-          {[...Array(5)].map(() => (
-            <Post
-              id={1}
-              title="Roast the code #1 | Rock Paper Scissors"
-              imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
-              user={{
-                avatarUrl:
-                  "https://res.cloudinary.com/practicaldev/image/fetch/s--uigxYVRB--/c_fill,f_auto,fl_progressive,h_50,q_auto,w_50/https://dev-to-uploads.s3.amazonaws.com/uploads/user/profile_image/187971/a5359a24-b652-46be-8898-2c5df32aa6e0.png",
-                fullName: "Keff",
-              }}
-              createdAt={"12 июня 2022 г."}
-              viewsCount={150}
-              commentsCount={3}
-              tags={["react", "fun", "typescript"]}
-              isEditable
-            />
-          ))}
+          {(isPostsLoading ? [...Array(5)] : posts.items).map((post, index) =>
+            isPostsLoading ? (
+              <Post key={index} isLoading />
+            ) : (
+              <Post
+                key={post._id}
+                id={post._id}
+                title={post.title}
+                imageUrl={post.imageUrl}
+                user={post.user}
+                createdAt={post.createdAt}
+                viewsCount={post.viewsCount}
+                commentsCount={3}
+                tags={post.tags}
+                isEditable
+              />
+            )
+          )}
         </Grid>
         <Grid xs={4} item>
           <TagsBlock
