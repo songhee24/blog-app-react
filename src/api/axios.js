7979cb0201fb24296a60c7198ba75dedff5fd3d7
songhee24 <akaratel61@gmail.com>
2023-08-16 23:01:19 +0600

    trying to implement http only logic

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,11 +1,17 @@
 import axios from "axios";
 
 const instance = axios.create({
+  withCredentials: true,
   baseURL: "http://localhost:4444",
 });
 
 instance.interceptors.request.use((config) => {
+  //TODO token will not stored in localstorage
   config.headers.Authorization = window.localStorage.getItem("token");
+  return config;
+});
+
+instance.interceptors.response.use((config) => {
   return config;
 });
 
