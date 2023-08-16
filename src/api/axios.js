79a483216ab27fe6ec7154b88baa55bbb6176439
songhee24 <akaratel61@gmail.com>
2023-08-17 04:14:11 +0600

    update axios config with credentials true logic

--- src/api/axios.js
+++ src/api/axios.js
@@ -4,10 +4,6 @@ const BASE_URL = "http://localhost:4444";
 
 export default axios.create({
   baseURL: BASE_URL,
-});
-
-export const axiosPrivate = axios.create({
-  baseURL: BASE_URL,
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
